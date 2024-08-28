a8258256a38ab388feaf617a53697b1342cc6ddf
arrKhange1 <therock65frostik@gmail.com>
2024-08-28 15:01:07 +0500

    init

---
+++ src/main/java/com/innovativesoftware/domsommelier_backend/DomsommelierBackendApplication.java
@@ -0,0 +1,13 @@
+package com.innovativesoftware.domsommelier_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomsommelierBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomsommelierBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innovativesoftware/domsommelier_backend/DomsommelierBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovativesoftware.domsommelier_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomsommelierBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
