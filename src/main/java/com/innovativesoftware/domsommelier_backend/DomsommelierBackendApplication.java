bef3fe32190f960b591444ab214bdb8311cf5bdd
arrKhange1 <therock65frostik@gmail.com>
2024-09-17 21:02:31 +0500

    feat(DS-70): add redis config

--- src/main/java/com/innovativesoftware/domsommelier_backend/DomsommelierBackendApplication.java
+++ src/main/java/com/innovativesoftware/domsommelier_backend/DomsommelierBackendApplication.java
@@ -2,12 +2,11 @@ package com.innovativesoftware.domsommelier_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class DomsommelierBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DomsommelierBackendApplication.class, args);
 	}
-
 }
--- src/main/java/com/innovativesoftware/domsommelier_backend/user/service/UserRecommendationsService.java
+++ src/main/java/com/innovativesoftware/domsommelier_backend/user/service/UserRecommendationsService.java
@@ -1,12 +1,21 @@
 package com.innovativesoftware.domsommelier_backend.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @Service
 public class UserRecommendationsService {
+    @Cacheable(cacheNames = "userRecommendationsCache", key = "#userId")
     public String getUserRecommendations(String userId) {
-        return userId;
+        try {
+            TimeUnit.SECONDS.sleep(3);
+            return "Amogus";
+        } catch(Exception e) {
+            return null;
+        }
     }
 }
