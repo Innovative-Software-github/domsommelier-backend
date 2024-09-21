5dc0244d042887efe2936b6a44619df27803b73a
arrKhange1 <therock65frostik@gmail.com>
2024-09-21 21:19:36 +0500

    fix(DS-70): add lost changes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,25 @@
-insert into product_category values
-('white'),
-('red'),
-('dry');
+insert into product_category values ('white'), ('red'), ('dry');
+insert into product_country values ('russia'), ('france'), ('usa');
+
+insert into product (id, article, name, quantity, initial_price, price, category_id, country_id) values ('293b85d0-f739-4856-a09b-8495f2157e4d', 'product1', 'product1', 100, 1000, 1500, 'white', 'france');
+insert into product (id, article, name, quantity, initial_price, price, category_id, country_id) values ('ccfe12c7-2a16-4482-9c5b-ca57260fc475', 'product2', 'product2', 100, 20000, 20100, 'red', 'usa');
+insert into product (id, article, name, quantity, initial_price, price, category_id, country_id) values ('f3e364ff-b656-44a3-a16e-2263e560345c', 'product3', 'product3', 400, 5000, 7500, 'dry', 'russia');
+
+insert into customer (id, iiko_id, first_name, second_name) values ('12c6f5bf-a1e2-4244-bea0-6700c7aee95f', 'djsiuiu348748347', 'alex', 'super');
+
+insert into order_status values ('pending'), ('done');
+insert into addresses (id, region, city) values ('3c383bd0-e867-40e1-a8ac-c75ee53f7544', 'permskiy kray', 'perm');
+
+insert into orders (id, address_id, order_status, customer_id) values ('d542aea6-34e6-4085-b23f-de41d8ca410f', '3c383bd0-e867-40e1-a8ac-c75ee53f7544', 'done', '12c6f5bf-a1e2-4244-bea0-6700c7aee95f');
+insert into orders (id, address_id, order_status, customer_id) values ('88f6f83f-bae6-436b-a891-a44deccc39e2', '3c383bd0-e867-40e1-a8ac-c75ee53f7544', 'done', '12c6f5bf-a1e2-4244-bea0-6700c7aee95f');
+insert into orders (id, address_id, order_status, customer_id) values ('e41ad356-2796-4d61-bb2d-553f1d699cb8', '3c383bd0-e867-40e1-a8ac-c75ee53f7544', 'done', '12c6f5bf-a1e2-4244-bea0-6700c7aee95f');
+
+
+insert into order_item (id, quantity, order_id, product_id) values ('b860d51d-504d-4c05-88e6-1dd45b16c433', 20, 'd542aea6-34e6-4085-b23f-de41d8ca410f', 'f3e364ff-b656-44a3-a16e-2263e560345c');
+insert into order_item (id, quantity, order_id, product_id) values ('a36ed3c1-00b8-4f1f-a4ce-4e864b38c1df', 10, 'd542aea6-34e6-4085-b23f-de41d8ca410f', 'ccfe12c7-2a16-4482-9c5b-ca57260fc475');
+
+insert into order_item (id, quantity, order_id, product_id) values ('01d0513c-8a5b-4551-8847-327f271a1827', 30, 'e41ad356-2796-4d61-bb2d-553f1d699cb8', 'ccfe12c7-2a16-4482-9c5b-ca57260fc475');
+insert into order_item (id, quantity, order_id, product_id) values ('1f37d332-b9c5-4065-8a19-d69c1cb49058', 30, 'e41ad356-2796-4d61-bb2d-553f1d699cb8', '293b85d0-f739-4856-a09b-8495f2157e4d');
+insert into order_item (id, quantity, order_id, product_id) values ('5a63bea2-7d4c-49f3-8b3e-c88ba5de7cfc', 30, 'e41ad356-2796-4d61-bb2d-553f1d699cb8', '293b85d0-f739-4856-a09b-8495f2157e4d');
+
+insert into order_item (id, quantity, order_id, product_id) values ('ec1c25cb-a5eb-495a-970d-43740da55b28', 15, '88f6f83f-bae6-436b-a891-a44deccc39e2', 'ccfe12c7-2a16-4482-9c5b-ca57260fc475');
