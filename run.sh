58350729fafb0b14bb8ced8a0f47fd769afa390a
arrKhange1 <therock65frostik@gmail.com>
2024-10-28 20:33:04 +0500

    refact(DS-113): change cd script

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
-rm -rf ~/projects/domsommelier-backend
-git clone git@github.com:Innovative-Software-github/domsommelier-backend.git ~/projects/domsommelier-backend
-cd ~/projects/domsommelier-backend
-
 docker rm -f $(sudo docker ps -a -q)
 docker rmi -f $(sudo docker images -q)
 docker-compose up --build -d
