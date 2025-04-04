977d6e69775254d22cf5bcb29e1704ef391cbd88
arrKhange1 <therock65frostik@gmail.com>
2025-04-04 20:36:47 +0500

    change run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if docker ps -a -q > /dev/null; then
-  docker rm -f $(docker ps -a -q)
-fi
+#if docker ps -a -q > /dev/null; then
+#  docker rm -f $(docker ps -a -q)
+#fi
+#
+#if docker images -q > /dev/null; then
+#  docker rmi -f $(docker images -q)
+#fi
 
-if docker images -q > /dev/null; then
-  docker rmi -f $(docker images -q)
-fi
-
-docker-compose up --build -d
+docker-compose up --force-recreate -d
